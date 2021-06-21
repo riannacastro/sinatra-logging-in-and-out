31f891a973540adcefc932ab2810ba0d31485833
Rianna <riannaxcastro@gmail.com>
2021-06-21 08:43:28 -1000

    commit tree bfe1cf4fb3aecb37e0b1d93bd903919b90972481

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(params)
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
+    @user = User.find_by_id(session[:user_id])
+    if @user 
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
   get '/logout' do
+    session.clear
+    redirect to '/'
 
   end
 
